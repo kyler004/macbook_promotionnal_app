b8599a2681e93da530037d0add69e83ccdc9b6f3
kyler004 <lowelrayannkenneyemtsa@gmail.com>
2026-02-17 17:09:46 +0100

    Initial commit with navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <header>
+        <nav>
+            <img src="/logo.svg" alt="Apple logo"></img>
+
+            <ul>
+                {[
+                    {label: 'Store'}, 
+                    {label: 'Mac'}, 
+                    {label: 'iPhone'}, 
+                    {label: 'Watch'}, 
+                    {label: 'Vision'}, 
+                    {label: 'AirPods'}
+                ].map((link) => (
+                    <li key={link.label}>
+                        <a href={link.label}>{link.label}</a>
+                    </li>
+                ))}
+            </ul>
+            
+            <div className="flex-center gap-3">
+                <button>
+                    <img src="/search.svg" alt="Search" />
+                </button>
+                <button>
+                    <img src="/cart.svg" alt="Cart" />
+                </button>
+            </div>
+        </nav>
+    </header>
+  )
+}
+
+export default Navbar
